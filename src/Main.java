77dd3ee523ebc931b0dee09e4f73108474e5bf6c
Devon Lee <devonlee111@gmail.com>
2017-06-01 18:32:16 -1000

    Plant Fixes and Plant Reproduction
    I've edited the genome of the plants, as well as anything that reads the genes, so that it carries the correct amount of information to build a plant. I have also done some testing to make sure that traits are passed down from the parent plants to the offspring.

--- src/Main.java
+++ src/Main.java
@@ -12,41 +12,20 @@ import javafx.scene.shape.Line;
 public class Main extends Application {
 	double pixPIn = 10; 
 	
-	double centerX = 450;
-	double centerY = 900;
+	double centerX = 500;
+	double centerY = 1000;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		Group root = new Group();
-		Scene scene = new Scene(root, 900, 900, Color.SKYBLUE);
+		Scene scene = new Scene(root, 1000, 1000, Color.SKYBLUE);
 	
-		//Plant test = new Plant("1111111011101111111111111111111111111111111000000000000000");
-		Plant test = new Plant();
-		System.out.println(test.stemLength() + " | " + test.leafSize() + " | " + test.stemAngle());
-		ArrayList<Point> points = test.processPlant();
-		int i = 0;
-		double x1 = 0;
-		double y1 = 0;
-		double x2 = 0;
-		double y2 = 0;
-		for (Point point : points) {
-			if (i % 2 == 0) {
-				x1 = point.x;
-				y1 = point.y;
-			}
-			if (i % 2 == 1) {
-				x2 = point.x;
-				y2 = point.y;
-				Line temp = new Line((x1 * pixPIn) + centerX, (-y1 * pixPIn) + centerY, (x2 * pixPIn) + centerX, (-y2 * pixPIn) + centerY);
-				root.getChildren().addAll(temp);
-				System.out.println(x1 + ", " + y1 + " | " + x2 + ", " + y2);
-			}
-			i++;
-		}
-		
+		Plant test1 = new Plant("11001000111011010000010110110110111100110111100110111011001010011010100110");
+		Plant test2 = new Plant("10111100111101101111110010011000000001110001011001101101110100111110000101");
+		Plant test = test1.reproduce(test2);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+		drawPlant(test, root);
 		/*
 		double botX, botY, endX, endY, topX, topY;
 		double theta = 0;
@@ -97,8 +76,35 @@ public class Main extends Application {
 		}
 	}
 	
-	public void drawPlant(Plant plant) {
-		
+	public void drawPlant(Plant plant, Group root) {
+		ArrayList<Point> points = plant.processPlant();
+		int i = 0;
+		double x1 = 0;
+		double y1 = 0;
+		double x2 = 0;
+		double y2 = 0;
+		for (Point point : points) {
+			if (i % 2 == 0) {
+				x1 = point.x;
+				y1 = point.y;
+			}
+			if (i % 2 == 1) {
+				x2 = point.x;
+				y2 = point.y;
+				Line temp = new Line((x1 * pixPIn) + centerX, (-y1 * pixPIn) + centerY, (x2 * pixPIn) + centerX, (-y2 * pixPIn) + centerY);
+				temp.setStroke(Color.BROWN);
+				temp.setStrokeWidth(10);
+				root.getChildren().addAll(temp);
+				System.out.println(x1 + ", " + y1 + " | " + x2 + ", " + y2);
+			}
+			i++;
+		}
+		ArrayList<Point> leaves = plant.getLeafPos();
+		for (Point leaf : leaves) {
+			Circle newLeaf = new Circle((leaf.x * pixPIn) + centerX, (-leaf.y * pixPIn) + centerY, (plant.leafSize() / 2) * pixPIn);
+			newLeaf.setFill(Color.GREEN);
+			root.getChildren().addAll(newLeaf);
+		}
 	}
 	
 	public static void main(String[] args) {
--- src/Plant.java
+++ src/Plant.java
@@ -2,29 +2,29 @@ import java.util.ArrayList;
 
 public class Plant implements Comparable<Plant>{
 	// Genome is represented by a 43 bit string.
-	// First gene from bits 0 - 2 represents stem length
-	// Second gene from bits 3 - 6 represents leaf size
-	// Third gene from bits 7 - 13 represents angle of new stems
-	// Fourth gene from bits 14 - 42 represents a array implemented heap for structure of plant
-	// Fifth gene from bits 43 - 57 represents whether an intersection of 2 branches contains a leaf
-	ArrayList<Point> leafPos = new ArrayList<Point>();
+	// First gene from bits 0 - 2 represents stem length.
+	// Second gene from bits 3 - 5 represents leaf size.
+	// Third gene from bits 6 - 12 represents angle of new stems.
+	// Fourth gene from bits 13 - 42 represents a array implemented heap for structure of plant.
+	// Fifth gene from bits 43 - 74 represents whether an intersection of 2 branches contains a leaf.
+	ArrayList<Point> leaves = new ArrayList<Point>();
 	private String genome = "";
-	public double fitness;
+	private double fitness;
 	
-	// Create a new random plant
+	// Create a new random plant.
 	public Plant() {
-		for (int i = 0; i < 43; i++) {
+		for (int i = 0; i < 74; i++) {
 			genome += Integer.toString((int)(Math.random() * 2));
 		}
 		fitness = Math.random();
-		System.out.println(genome.substring(14, 43));
 	}
 	
+	// Create a new plant with a predetermined genome.
 	public Plant(String genome) {
 		this.genome = genome;
-		System.out.println(genome);
 	}
 	
+	// Comparison for the sorting of plants based on their fitness.
 	@Override
 	public int compareTo(Plant p) {
 		if (p.fitness < fitness) {
@@ -36,22 +36,21 @@ public class Plant implements Comparable<Plant>{
 		return 1;
 	}
 	
-	// Produce a new plant by combining genomes and mutating
+	// Produce a new plant by combining the genomes of different plants and mutating them.
 	public Plant reproduce(Plant mate) {
 		String newGenome = "";
-		String genome2 = mate.genome;
 		String gene11, gene12, gene21, gene22, gene31, gene32, gene41, gene42, gene51, gene52;
 		gene11 = genome.substring(0, 3);
-		gene21 = genome.substring(3, 7);
-		gene31 = genome.substring(7, 14);
-		gene41 = genome.substring(14, 43);
-		gene51 = genome.substring(43);
+		gene21 = genome.substring(3, 6);
+		gene31 = genome.substring(6, 13);
+		gene41 = genome.substring(13, 42);
+		gene51 = genome.substring(42);
 		
 		gene12 = mate.genome.substring(0, 3);
-		gene22 = mate.genome.substring(3, 7);
-		gene32 = mate.genome.substring(7, 14);
-		gene42 = mate.genome.substring(14, 43);
-		gene52 = mate.genome.substring(43);
+		gene22 = mate.genome.substring(3, 6);
+		gene32 = mate.genome.substring(6, 13);
+		gene42 = mate.genome.substring(13, 42);
+		gene52 = mate.genome.substring(42);
 		
 		newGenome += combineGenes(gene11, gene12);
 		newGenome += combineGenes(gene21, gene22);
@@ -59,7 +58,8 @@ public class Plant implements Comparable<Plant>{
 		newGenome += combineGenes(gene41, gene42);
 		newGenome += combineGenes(gene51, gene52);
 		char[] genomeArr = newGenome.toCharArray();
-		for (int i = 0; i < 58; i++) {
+		for (int i = 0; i < 74; i++) {
+			// Have a 1% chance of mutating any single bit.
 			if ((int)(Math.random() * 101) == 0) {
 				if (genomeArr[i] == '1') {
 					genomeArr[i] = '0';
@@ -74,6 +74,7 @@ public class Plant implements Comparable<Plant>{
 		return offspring;
 	}
 	
+	// Given 2 genes take half of one gene, starting at a random point, and swap it to the same position in the other gene.
 	public String combineGenes(String gene1, String gene2) {
 		char[] g1 = gene1.toCharArray();
 		char[] g2 = gene2.toCharArray();
@@ -89,17 +90,18 @@ public class Plant implements Comparable<Plant>{
 	}
 	
 	public int stemLength() {
-		return Integer.parseInt(genome.substring(0, 3), 2) + 3;
+		return Integer.parseInt(genome.substring(0, 3), 2) + 7;
 	}
 	
 	public int leafSize() {
-		return Integer.parseInt(genome.substring(3,  7), 2) + 1;
+		return Integer.parseInt(genome.substring(3,  6), 2) + 1;
 	}
 	
 	public int stemAngle() {
-		return Integer.parseInt(genome.substring(7, 14), 2) + 1;
+		return Integer.parseInt(genome.substring(6, 13), 2) + 1;
 	}
 	
+	// Process the genome to get the structure of the plant
 	public ArrayList<Point> processPlant() {
 		ArrayList<Point> positions = new ArrayList<Point>();
 		Point start = new Point(0, 0);
@@ -107,13 +109,20 @@ public class Plant implements Comparable<Plant>{
 		return positions;
 	}
 	
+	public ArrayList<Point> getLeafPos() {
+		return leaves;
+	}
+	
+	// Recursively step through the part of the genome that indicates the structure of the plant.
+	// Add the starting and ending points of each branch to the given ArrayList.
+	// Add the positions of the leaves to the stored ArrayList.
 	public void getBranchPositions(ArrayList<Point> positions, int index, Point startPoint, int theta, int LR) {
-		//System.out.print(theta + " | ");
-		String gene = genome.substring(14, 43);
-		System.out.println(index);
+		String gene = genome.substring(13, 43);
+		String leafGene = genome.substring(43);
 		int leftIndex = (index * 2);
 		int rightIndex = (index * 2) + 1;
 		double x, y, thetaR;
+		
 		switch(LR) {
 		case -1:
 			theta += stemAngle();
@@ -126,11 +135,10 @@ public class Plant implements Comparable<Plant>{
 			}
 			break;
 		}
-		//System.out.println(LR + " | " + theta);
+		
 		thetaR = theta * (Math.PI / 180);
 		x = Math.cos(thetaR);
 		y = Math.sin(thetaR);
-		//System.out.println(x + ", " + y);
 		
 		if (Math.abs(x) < 0.00001) {
 			x = 0;
@@ -157,19 +165,20 @@ public class Plant implements Comparable<Plant>{
 		
 		x *= stemLength();
 		y *= stemLength();
-		//System.out.println(x + ", " + y);
 		x += startPoint.x;
 		y += startPoint.y;
-		//System.out.println("starting point is " + startPoint.x + ", " + startPoint.y + " | end point is " + x + ", " + y);
 		Point endPoint = new Point(x, y);
 		positions.add(startPoint);
 		positions.add(endPoint);
-		if (leftIndex <= 14 && gene.charAt(leftIndex - 2) == '1') {
-			//System.out.println("left " + leftIndex);
+		
+		if (leafGene.charAt(index - 1) == '1' && (y - (leafSize()/2)) > 0) {
+			leaves.add(endPoint);
+		}
+		
+		if (leftIndex <= 30 && gene.charAt(leftIndex - 2) == '1' && endPoint.y > 0) {
 			getBranchPositions(positions, leftIndex, endPoint, theta, -1);
 		}
-		if (rightIndex <= 15 && gene.charAt(rightIndex - 2) == '1') {
-			//System.out.println("right " + rightIndex);
+		if (rightIndex <= 31 && gene.charAt(rightIndex - 2) == '1' && endPoint.y > 0) {
 			getBranchPositions(positions, rightIndex, endPoint, theta, 1);
 		}
 	}
