52f9faacab0eb682a07391665ca1eec427f7472b
Devon Lee <devonlee111@gmail.com>
2017-06-10 16:33:47 -1000

    Redesigning Plants
    I took some time to rethink how I wanted to represent the genome of plants. I realized that the way I had represented plants before was a bit strange and led to fairly simplistic and repetative plants. With this redesign a plant's genome will be comprised of genes that represent individual branches, each with their own attributes (ie. branch angle, branch length), instead of having one set of attributes per plant. This change allows the plant structure to be determined by the combination of all the genes instead of a single gene. Overall these changes should allow for more varied plants with less uniform shapes.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.awt.geom.Line2D;
-import java.util.ArrayList;
-import java.util.Arrays;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -10,8 +7,7 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 public class Main extends Application {
-	double pixPIn = 10; 
-	
+	double pxUnit = 5;
 	double centerX = 500;
 	double centerY = 1000;
 	
@@ -19,91 +15,52 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		Group root = new Group();
 		Scene scene = new Scene(root, 1000, 1000, Color.SKYBLUE);
-	
-		Plant test1 = new Plant("11001000111011010000010110110110111100110111100110111011001010011010100110");
-		Plant test2 = new Plant("10111100111101101111110010011000000001110001011001101101110100111110000101");
-		Plant test = test1.reproduce(test2);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		drawPlant(test, root);
-		/*
-		double botX, botY, endX, endY, topX, topY;
-		double theta = 0;
-		double thetaD;
-		
-		for (int i = 0; i < 91; i++) {
-			thetaD = theta * (Math.PI / 180);
-			topX = (0 + 7 * Math.cos(thetaD));
-			topY = (0 - 7 * Math.sin(thetaD));
-			botX = (0 + 7 * Math.cos(thetaD));
-			botY = (0 - 7 * Math.sin(thetaD));
-			endX = botX + (150 * Math.sin(thetaD));
-			endY = botY + (150 * Math.cos(thetaD));
-			
-			System.out.print("theta is " + thetaD);
-			System.out.print("| Bottom is " + botX + ", " + botY);
-			System.out.println("| Top is " + topX + ", " +  topY);
-			theta += (1);
-			}
-		}
-		/*
-		root.getChildren().addAll(new Line(botX  * pixPIn, botY * pixPIn, endX * pixPIn, endY * pixPIn));
-		
-		Line2D line = new Line2D.Double(botX, botY, endX, endY);
-		MyCircle circle = new MyCircle(60, 60, 7);
-		circle.intersect(line);
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		Plant[] generation = new Plant[100];
-		for(int i = 0; i < 100; i++) {
-			Plant plant = new Plant();
-			generation[i] = plant;
-		}
-		for (Plant plant : generation) {
-			calculateFitness(plant);
-		}
-		Arrays.sort(generation);
-		System.out.println(generation[0].stemLength() + " | " + generation[0].leafSize() + " | " + generation[0].stemAngle());
-		*/
+		Plant p1 = new Plant(10);
+		Plant p2 = new Plant(11);
+		Plant child = new Plant(12, p1, p2);
+		child.branchLength(0);
+		drawPlant(child, root);
 	}
 	
 	public void calculateFitness(Plant plant) {
-		double thetaD = -90;
-		double theta;
-		for (int i = 0; i < 180; i++) {
-			
-		}
+
 	}
 	
 	public void drawPlant(Plant plant, Group root) {
-		ArrayList<Point> points = plant.processPlant();
-		int i = 0;
-		double x1 = 0;
-		double y1 = 0;
-		double x2 = 0;
-		double y2 = 0;
-		for (Point point : points) {
-			if (i % 2 == 0) {
-				x1 = point.x;
-				y1 = point.y;
+		double theta, length;
+		Point startPos = plant.groundPos();
+		Point endPos;
+		for (int i = 0; i < 20; i++) {
+			int geneType = plant.geneType(i * 10);
+			if (geneType == 0) {
+				theta = plant.branchAngle(i * 10);
+				length = plant.branchLength(i * 10);
+				theta += 90;
+				System.out.println(theta + ", " + length);
+				theta *= (Math.PI / 180);
+				endPos = new Point(-(length * Math.cos(theta) * pxUnit) + startPos.x + centerX, -(length * Math.sin(theta) * pxUnit) + startPos.y + centerY);
+				Line line = new Line(startPos.x + centerX, startPos.y + centerY, endPos.x, endPos.y);
+				line.setStroke(Color.BROWN);
+				line.setStrokeWidth(10);
+				root.getChildren().addAll(line);
+				startPos = endPos;
 			}
-			if (i % 2 == 1) {
-				x2 = point.x;
-				y2 = point.y;
-				Line temp = new Line((x1 * pixPIn) + centerX, (-y1 * pixPIn) + centerY, (x2 * pixPIn) + centerX, (-y2 * pixPIn) + centerY);
-				temp.setStroke(Color.BROWN);
-				temp.setStrokeWidth(10);
-				root.getChildren().addAll(temp);
-				System.out.println(x1 + ", " + y1 + " | " + x2 + ", " + y2);
+			else if (geneType == 1) {
+				theta = plant.branchAngle(i * 10);
+				length = plant.branchLength(i * 10);
+				theta += 90;
+				System.out.println(theta + ", " + length);
+				theta *= (Math.PI / 180);
+				endPos = new Point(-(length * Math.cos(theta) * pxUnit) + startPos.x + centerX, -(length * Math.sin(theta) * pxUnit) + startPos.y + centerY);
+				Line line = new Line(startPos.x + centerX, startPos.y + centerY, endPos.x, endPos.y);
+				line.setStroke(Color.BROWN);
+				line.setStrokeWidth(10);
+				Circle leaf = new Circle(endPos.x, endPos.y, 20);
+				leaf.setFill(Color.GREEN);
+				root.getChildren().addAll(line, leaf);
 			}
-			i++;
-		}
-		ArrayList<Point> leaves = plant.getLeafPos();
-		for (Point leaf : leaves) {
-			Circle newLeaf = new Circle((leaf.x * pixPIn) + centerX, (-leaf.y * pixPIn) + centerY, (plant.leafSize() / 2) * pixPIn);
-			newLeaf.setFill(Color.GREEN);
-			root.getChildren().addAll(newLeaf);
 		}
 	}
 	
--- src/Plant.java
+++ src/Plant.java
@@ -1,27 +1,53 @@
 import java.util.ArrayList;
 
-public class Plant implements Comparable<Plant>{
-	// Genome is represented by a 43 bit string.
-	// First gene from bits 0 - 2 represents stem length.
-	// Second gene from bits 3 - 5 represents leaf size.
-	// Third gene from bits 6 - 12 represents angle of new stems.
-	// Fourth gene from bits 13 - 42 represents a array implemented heap for structure of plant.
-	// Fifth gene from bits 43 - 74 represents whether an intersection of 2 branches contains a leaf.
-	ArrayList<Point> leaves = new ArrayList<Point>();
+public class Plant implements Comparable<Plant> {
+	private String trunkCode = "AGTT";
+	private String branchCode = "GCGA";
+	private String nucleotides = "ATCG";
+	private ArrayList<Point> leaves = new ArrayList<Point>();
 	private String genome = "";
+	private double groundPos;
 	private double fitness;
 	
-	// Create a new random plant.
-	public Plant() {
-		for (int i = 0; i < 74; i++) {
-			genome += Integer.toString((int)(Math.random() * 2));
+	// Create a new plant with a basic genome.
+	public Plant(double groundPos) {
+		this.groundPos = groundPos;
+		genome += branchCode;
+		genome += "GGAAAA";
+		for (int i = 0; i < 190; i++) {
+			genome += 'A';
 		}
-		fitness = Math.random();
+		System.out.println(genome);
 	}
 	
-	// Create a new plant with a predetermined genome.
-	public Plant(String genome) {
-		this.genome = genome;
+	// Create a new plant from the genes of 2 parent plants
+	public Plant(double groundPos, Plant p1, Plant p2) {
+		// Randomly choose genes from each parent to put into the offspring's genes.
+		this.groundPos = groundPos;
+		for (int i = 0; i < 20; i++) {
+			int geneIndex = 10 * i;
+			int parent = (int)(Math.random() * 100) + 1;
+			if (parent <= 50) {
+				genome += p1.genome.substring(geneIndex, geneIndex + 10);
+			}
+			else {
+				genome += p2.genome.substring(geneIndex, geneIndex + 10);
+			}
+		}
+		// Give each nucleotide in the plant a very small chance to mutate into a different nucleotide.
+		char[] temp = genome.toCharArray();
+		for (int i = 0; i < genome.length(); i++) {
+			int mutationChance = (int)(Math.random() * 1000);
+			if (mutationChance == 0) {
+				char newNucleotide;
+				do {
+					newNucleotide = nucleotides.charAt((int)(Math.random() * 4));
+				} while(newNucleotide != temp[i]);
+				temp[i] = newNucleotide;
+			}
+		}
+		genome = new String(temp);
+		System.out.println(genome);
 	}
 	
 	// Comparison for the sorting of plants based on their fitness.
@@ -36,150 +62,56 @@ public class Plant implements Comparable<Plant>{
 		return 1;
 	}
 	
-	// Produce a new plant by combining the genomes of different plants and mutating them.
-	public Plant reproduce(Plant mate) {
-		String newGenome = "";
-		String gene11, gene12, gene21, gene22, gene31, gene32, gene41, gene42, gene51, gene52;
-		gene11 = genome.substring(0, 3);
-		gene21 = genome.substring(3, 6);
-		gene31 = genome.substring(6, 13);
-		gene41 = genome.substring(13, 42);
-		gene51 = genome.substring(42);
-		
-		gene12 = mate.genome.substring(0, 3);
-		gene22 = mate.genome.substring(3, 6);
-		gene32 = mate.genome.substring(6, 13);
-		gene42 = mate.genome.substring(13, 42);
-		gene52 = mate.genome.substring(42);
-		
-		newGenome += combineGenes(gene11, gene12);
-		newGenome += combineGenes(gene21, gene22);
-		newGenome += combineGenes(gene31, gene32);
-		newGenome += combineGenes(gene41, gene42);
-		newGenome += combineGenes(gene51, gene52);
-		char[] genomeArr = newGenome.toCharArray();
-		for (int i = 0; i < 74; i++) {
-			// Have a 1% chance of mutating any single bit.
-			if ((int)(Math.random() * 101) == 0) {
-				if (genomeArr[i] == '1') {
-					genomeArr[i] = '0';
-				}
-				else {
-					genomeArr[i] = '1';
-				}
-			}
+	public void setFitness(double fitness) {
+		this.fitness = fitness;
+	}
+	
+	public Point groundPos() {
+		return new Point(groundPos, 0);
+	}
+	
+	public int geneType(int geneIndex) {
+		if(genome.substring(geneIndex, geneIndex + 4).equals(branchCode)) {
+			return 1;
 		}
-		newGenome = new String(genomeArr);
-		Plant offspring = new Plant(newGenome);
-		return offspring;
-	}
-	
-	// Given 2 genes take half of one gene, starting at a random point, and swap it to the same position in the other gene.
-	public String combineGenes(String gene1, String gene2) {
-		char[] g1 = gene1.toCharArray();
-		char[] g2 = gene2.toCharArray();
-		int geneLength = gene1.length();
-		int numSwap = geneLength/2;
-		int startPos = (int)(Math.random() * ((geneLength - numSwap) + 1));
-		int endPos = startPos + (numSwap);
-		for (int i = startPos; i < endPos; i++) {
-			g1[i] = g2[i];
+		if(genome.substring(geneIndex, geneIndex + 4).equals(trunkCode)) {
+			return 0;
 		}
-		String newGene = new String(g1);
-		return newGene;
+		return -1;
 	}
 	
-	public int stemLength() {
-		return Integer.parseInt(genome.substring(0, 3), 2) + 7;
+	public double branchAngle(int geneIndex) {
+		double angle = 0.0;
+		char[] nucleotides = genome.substring(geneIndex + 6, geneIndex + 10).toCharArray();
+		String temp = "";
+		for (int i = 0; i < 4; i++) {
+			temp += Integer.toString(this.nucleotides.indexOf(nucleotides[i]));
+		}
+		angle = Integer.parseInt(temp, 4);
+		System.out.println(angle);
+		if (angle > 127) {
+			angle = 128 - angle;
+		}
+		return angle;
 	}
 	
-	public int leafSize() {
-		return Integer.parseInt(genome.substring(3,  6), 2) + 1;
+	public double branchLength(int geneIndex) {
+		double length = 0.0;
+		char[] nucleotides = genome.substring(geneIndex + 4, geneIndex + 6).toCharArray();
+		String temp = "";
+		for (int i = 0; i < 2; i++) {
+			temp += Integer.toString(this.nucleotides.indexOf(nucleotides[i]));
+		}
+		length = Integer.parseInt(temp, 4);
+		return length + 1;
 	}
-	
-	public int stemAngle() {
-		return Integer.parseInt(genome.substring(6, 13), 2) + 1;
-	}
-	
+
 	// Process the genome to get the structure of the plant
 	public ArrayList<Point> processPlant() {
-		ArrayList<Point> positions = new ArrayList<Point>();
-		Point start = new Point(0, 0);
-		getBranchPositions(positions, 1, start, 90, 0);
-		return positions;
+		return null;
 	}
 	
 	public ArrayList<Point> getLeafPos() {
 		return leaves;
-	}
-	
-	// Recursively step through the part of the genome that indicates the structure of the plant.
-	// Add the starting and ending points of each branch to the given ArrayList.
-	// Add the positions of the leaves to the stored ArrayList.
-	public void getBranchPositions(ArrayList<Point> positions, int index, Point startPoint, int theta, int LR) {
-		String gene = genome.substring(13, 43);
-		String leafGene = genome.substring(43);
-		int leftIndex = (index * 2);
-		int rightIndex = (index * 2) + 1;
-		double x, y, thetaR;
-		
-		switch(LR) {
-		case -1:
-			theta += stemAngle();
-			theta = theta % 360;
-			break;
-		case 1:
-			theta -= stemAngle();
-			if (theta < 0) {
-				theta = 360 + theta;
-			}
-			break;
-		}
-		
-		thetaR = theta * (Math.PI / 180);
-		x = Math.cos(thetaR);
-		y = Math.sin(thetaR);
-		
-		if (Math.abs(x) < 0.00001) {
-			x = 0;
-		}
-		if (Math.abs(y) < 0.00001) {
-			y = 0;
-		}
-		if (1 - Math.abs(x) < 0.00001) {
-			if (x < 1) {
-				x = -1;
-			}
-			else {
-				x = 1;
-			}
-		}
-		if (1 - Math.abs(y) < 0.00001) {
-			if (y < 1) {
-				y = -1;
-			}
-			else {
-				y = 1;
-			}
-		}
-		
-		x *= stemLength();
-		y *= stemLength();
-		x += startPoint.x;
-		y += startPoint.y;
-		Point endPoint = new Point(x, y);
-		positions.add(startPoint);
-		positions.add(endPoint);
-		
-		if (leafGene.charAt(index - 1) == '1' && (y - (leafSize()/2)) > 0) {
-			leaves.add(endPoint);
-		}
-		
-		if (leftIndex <= 30 && gene.charAt(leftIndex - 2) == '1' && endPoint.y > 0) {
-			getBranchPositions(positions, leftIndex, endPoint, theta, -1);
-		}
-		if (rightIndex <= 31 && gene.charAt(rightIndex - 2) == '1' && endPoint.y > 0) {
-			getBranchPositions(positions, rightIndex, endPoint, theta, 1);
-		}
 	}
 }
